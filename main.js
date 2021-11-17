d65358141f6b88bbe542e81210fed60fd7fead7b
Andrej <andrej.tlcina23@gmail.com>
2021-11-17 14:29:07 +0100

    feat(device_motion): add confirm modal

--- main.js
+++ main.js
@@ -56,16 +56,30 @@ function addMotionListener() {
   });
 }
 
-if (typeof DeviceMotionEvent.requestPermission === "function") {
-  // iOS 13+
-  DeviceMotionEvent.requestPermission()
+function ClickRequestDeviceMotionEvent() {
+  window.DeviceMotionEvent.requestPermission()
     .then((response) => {
-      if (response == "granted") {
+      if (response === "granted") {
         addMotionListener();
+      } else {
+        console.log("DeviceMotion permissions not granted.");
       }
     })
-    .catch(console.error);
-} else {
-  // non iOS 13+
-  addMotionListener();
+    .catch((e) => {
+      console.error(e);
+    });
 }
+
+window.onload = function () {
+  // Check if is IOS 13 when page loads.
+  if (
+    window.DeviceMotionEvent &&
+    typeof window.DeviceMotionEvent.requestPermission === "function"
+  ) {
+    // Everything here is just a lazy banner. You can do the banner your way.
+    const banner = document.createElement("div");
+    banner.innerHTML = `<div style="z-index: 1; position: absolute; width: 100%; background-color:#000; color: #fff"><p style="padding: 10px">Click here to enable DeviceMotion</p></div>`;
+    banner.onclick = ClickRequestDeviceMotionEvent; // You NEED to bind the function into a onClick event. An artificial 'onClick' will NOT work.
+    document.querySelector("body").appendChild(banner);
+  }
+};
